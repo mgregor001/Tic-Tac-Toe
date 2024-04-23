95ac8c059f71e24fb148b3d95ff51bd808ecc2c9
TimbyJake63 <jtimberlake002@student.franciscan.edu>
2024-04-23 11:19:18 -0400

    commit

--- TicTacToe Main Code/Main Code.py
+++ TicTacToe Main Code/Main Code.py
@@ -5,15 +5,45 @@ frame.pack()
 window.columnconfigure([0, 1, 2], weight=1, minsize=50)
 window.rowconfigure([0, 1, 2], weight=1, minsize=50)
 
-btn_1 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_2 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_3 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_4 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_5 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_6 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_7 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_8 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
-btn_9 = tk.Button(master=frame, width=7, height=3, padx=5, pady=5)
+lbl_amount = tk.Label(master=window, text="")
+lbl_amount.pack()
+
+def handler_1():
+    lbl_amount['text'] += " "
+
+def handler_2():
+    lbl_amount['text'] += " "
+
+def handler_3():
+    lbl_amount['text'] += " "
+
+def handler_4():
+    lbl_amount['text'] += " "
+
+def handler_5():
+    lbl_amount['text'] += " "
+
+def handler_6():
+    lbl_amount['text'] += " "
+
+def handler_7():
+    lbl_amount['text'] += " "
+
+def handler_8():
+    lbl_amount['text'] += " "
+
+def handler_9():
+    lbl_amount['text'] += " "
+
+btn_1 = tk.Button(master=frame, command=handler_1, width=7, height=3, padx=5, pady=5)
+btn_2 = tk.Button(master=frame, command=handler_2, width=7, height=3, padx=5, pady=5)
+btn_3 = tk.Button(master=frame, command=handler_3, width=7, height=3, padx=5, pady=5)
+btn_4 = tk.Button(master=frame, command=handler_4, width=7, height=3, padx=5, pady=5)
+btn_5 = tk.Button(master=frame, command=handler_5, width=7, height=3, padx=5, pady=5)
+btn_6 = tk.Button(master=frame, command=handler_6, width=7, height=3, padx=5, pady=5)
+btn_7 = tk.Button(master=frame, command=handler_7, width=7, height=3, padx=5, pady=5)
+btn_8 = tk.Button(master=frame, command=handler_8, width=7, height=3, padx=5, pady=5)
+btn_9 = tk.Button(master=frame, command=handler_9, width=7, height=3, padx=5, pady=5)
 
 btn_1.grid(row=0, column=0, sticky="nsew")
 btn_2.grid(row=1, column=0, sticky="nsew")
@@ -25,8 +55,8 @@ btn_7.grid(row=0, column=2, sticky="nsew")
 btn_8.grid(row=1, column=2, sticky="nsew")
 btn_9.grid(row=2, column=2, sticky="nsew")
 
-bool=True
-if bool is True:
+# bool=True
+# if bool is True:
     
 
 window.mainloop()
